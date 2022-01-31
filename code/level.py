055d2884b7ac16541c2ed65742546ec5fbb1974f
Rhys Huxley <rhyshuxley@gmail.com>
2022-01-31 20:48:55 +0000

    Add player movement and collisions

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -22,8 +23,9 @@ class Level:
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
 
                 if column == 'p':
-                    Player((x, y), [self.visible_sprites])
-
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
--- code/player.py
+++ code/player.py
@@ -2,7 +2,60 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, position, groups):
+    def __init__(self, position, groups, obstacles):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacles = obstacles
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for obstacle in self.obstacles:
+                if obstacle.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = obstacle.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = obstacle.rect.right
+
+        if direction == 'vertical ':
+            for obstacle in self.obstacles:
+                if obstacle.rect.colliderect(self.rect):
+                    debug(self.rect.y)
+                    if self.direction.y > 0:
+                        self.rect.bottom = obstacle.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = obstacle.rect.bottom
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
